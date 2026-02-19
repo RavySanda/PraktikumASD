38de69fdc3ce5787dff11e41f0a97c89f3978cab
RavySanda <ravyadityaa@gmail.com>
2026-02-19 19:39:31 +0700

    tugas 1

---
+++ Tugas1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] kode = {"A","B","D","E","F","G","H","L","N","T"};
+
+        String[] kota = {
+            "BANTEN",
+            "JAKARTA",
+            "BANDUNG",
+            "CIREBON",
+            "BOGOR",
+            "PEKALONGAN",
+            "SEMARANG",
+            "SURABAYA",
+            "MALANG",
+            "TEGAL"
+        };
+
+        while (true) {  
+
+            System.out.print("\nMasukkan kode plat nomor: ");
+            String input = sc.next().toUpperCase();
+
+            boolean ketemu = false;
+
+            for (int i = 0; i < kode.length; i++) {
+                if (input.equals(kode[i])) {
+                    System.out.println("Nama kota : " + kota[i]);
+                    ketemu = true;
+                    break;
+                }
+            }
+
+            if (!ketemu) {
+                System.out.println("Kode plat tidak ditemukan");
+            }
+        }
+    }
+}
