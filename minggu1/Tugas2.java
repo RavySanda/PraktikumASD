1c418c5ee025a01c8b97590d8e4caa457d5dc404
RavySanda <ravyadityaa@gmail.com>
2026-02-20 11:55:31 +0700

    jobsheet 1

--- minggu1/Tugas2.java
+++ minggu1/Tugas2.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 
 public class Tugas2{
-
     static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         System.out.print("Masukkan jumlah jadwal kuliah: ");
