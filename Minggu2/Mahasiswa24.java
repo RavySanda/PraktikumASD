c8c2366664969f570a6ab5d61fa3642dcfad2f34
RavySanda <ravyadityaa@gmail.com>
2026-02-20 11:27:43 +0700

    JOBSHEET 2

---
+++ Minggu2/Mahasiswa24.java
@@ -0,0 +1,41 @@
+
+public class Mahasiswa24 {
+
+    
+String nama;
+String nim;
+String kelas;
+double ipk; 
+
+void tampilkanInformasi () {
+  System.out.println("Nama : " + nama);
+  System.out.println("NIM : " + nim);
+  System.out.println("IPK : " + ipk);
+  System.out.println("Kelas : " + kelas);
+}
+void ubahKelas(String kelasBaru){
+    kelas = kelasBaru;
+}
+void updateIpk(double ipkBaru){
+        if(ipkBaru >= 0.0 && ipkBaru <= 4.0){
+            ipk = ipkBaru;
+        }else {
+            System.out.println("IPK tidak valid. Harus anatara 0.0 dan 4.0");       
+        }
+}
+String nilaiKinerja(double ipk) {
+        if (ipk >= 3.5) {
+            return "Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Baik";
+        } else if (ipk >= 2.5) {
+            return "Cukup";
+        } else if (ipk >= 2.0) {
+            return "Kurang";
+        } else {
+            return "Sangat Kurang";
+        }
+    }
+}
+
+
---
+++ Minggu2/MahasiswaMain2.java
@@ -0,0 +1,19 @@
+
+public class MahasiswaMain2 {
+    public static void main(String[] args) {
+        
+        Mahasiswa24 mhs1 = new Mahasiswa24();
+        mhs1.nama = "Ravy Sanda Aditya";
+        mhs1.nim = "254107020211";
+        mhs1.kelas = "TI 1D";
+        mhs1.ipk = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("TI 1C");
+        mhs1.updateIpk(4.2);
+        mhs1.tampilkanInformasi();
+
+
+    }
+    
+}
